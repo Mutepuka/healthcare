4ec62a836b31514420629ffe8adb5f6e96e93161
Musonda Makena <Musondamakena@gmail.com>
2024-08-12 20:06:03 +0200

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import PatientForm from "@/components/form/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +15,21 @@ export default function Home() {
           alt="Pateint"
           className="mb-12 h-10 w-fit"
           />
+          <PatientForm/>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left"> © 2024 HealthCare</p>
+            <Link href="/?admin=true" className="text-green-500">Admin</Link>
+         
+          </div>
         </div>
       </section>
+      <Image
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="Patient"
+      className="side-img max-w-[50%]"
+      />
       
     </div>
    
